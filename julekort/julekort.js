3d592e7beab1cf8cbc4111ecd3f6c1b7b37c94df
henrikskatla <henrik.s.katla@gmail.com>
2017-12-19 09:31:10 +0100

    snøkanon

--- julekort/julekort.js
+++ julekort/julekort.js
@@ -2,9 +2,9 @@ function setup() {
     let divSky = document.getElementById("sky");
     let divSanta = document.getElementById("santa");    
     let stars = divSky.querySelectorAll(".star");
-    stars.forEach( stjerne => {
-        stjerne.style.left = -120 + Math.floor(Math.random()*240) + "px";
-        stjerne.style.top = -80 + Math.floor(Math.random()*160) + "px";        
+    stars.forEach(stjerne => {
+        stjerne.style.left = -120 + Math.floor(Math.random() * 240) + "px";
+        stjerne.style.top = -80 + Math.floor(Math.random() * 160) + "px";
     });
 
     divSanta.addEventListener("click" , dropGifts);
@@ -16,4 +16,33 @@ function setup() {
         pakke.style.top = (e.screenY - 50) + "px";        
         divSky.appendChild(pakke);
     }
+
+    function makeSnow() {
+        for (let i = 0; i < 254; i++) {
+            let snow = document.createElement('div');
+            snow.className = "snow";
+            snow.style.left = Math.random()*100 + "vw";
+            snow.style.animationDelay = Math.random() * 5000 + "ms";
+            divSky.appendChild(snow);
+        }
+    }
+
+    makeSnow();
+    
+
+
+    function cannonSnow() {
+        for (let i = 0; i < 44; i++) {
+            let snow = document.createElement('div');
+            snow.className = "powder";
+            snow.style.left = Math.random()*10 + "vw";
+            snow.style.animationDelay = Math.random() * 5000 + "ms";
+            let radius = Math.random() * 10 + 1;
+            snow.style.width = radius + "px";
+            snow.style.height = radius + "px";            
+            divSky.appendChild(snow);
+        }
+    }
 }
+
+makeSnow();
