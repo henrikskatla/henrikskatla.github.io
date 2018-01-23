9e6636a3d2fb2e52e3bbc660bc808df0b7b06275
henrikskatla <henrik.s.katla@gmail.com>
2018-01-23 09:51:44 +0100

    bilverksted

--- risk/rediger.js
+++ risk/rediger.js
@@ -1,4 +1,6 @@
-function setup(){
+// @ts-check
+
+function setup() {
     var config = {
         apiKey: "AIzaSyDoJ7SgRSRv2yBSNFe8ixL3fnxEDTTdXhM",
         authDomain: "risk-repitisjon.firebaseapp.com",
@@ -6,21 +8,51 @@ function setup(){
         projectId: "risk-repitisjon",
         storageBucket: "",
         messagingSenderId: "53266940999"
-      };
-      firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
+
+    let database = firebase.database();
+
+    let inpLand = document.getElementById("land");
+    let inpRegion = document.getElementById("region");
+
+    let btnLagreLand = document.getElementById("lagreland");
+    btnLagreLand.addEventListener("click", lagreLand);
     
-      let database = firebase.database();
+    let inpKort = document.getElementById("kort");
+    let inpAntallNye = document.getElementById("antallnye");
 
-      let inpLand = document.getElementById("land");
-      let inpRegion = document.getElementById("region");
+    let btnLagreKort = document.getElementById("lagrekort");
+    btnLagreKort.addEventListener("click", lagreKort);
+    
+    function lagreLand(e) {
+        let land = inpLand.value;
+        let kortid = inpRegion.value;
+        let ref = database.ref("land/" + land);
+        ref.set({ kortid });
+    }
 
-      let btnLagre = document.getElementById("lagre");
-      btnLagre.addEventListener("click", lagreData);
+    function lagreKort(e) {
+        let kort = inpKort.value;
+        let antallnye = inpAntallNye.value;
+        let ref = database.ref("kort/" + kort);
+        ref.set({ antallnye });
+    }
 
-      function lagreData (e) {
-          let land = inpLand.value;
-          let region = inpRegion.value;
-          let ref = database.ref("land/" + land);
-          ref.set( { kortid});
-      }
+    let inpBruker = document.getElementById("bruker");
+    let inpAlder = document.getElementById("alder");
+    let inpFarge = document.getElementById("farge");
+    let inpNavn = document.getElementById("navn");
+
+    let btnLagreSpiller = document.getElementById("lagrespiller");
+    btnLagreSpiller.addEventListener("click", lagreSpiller);
+
+    function lagreSpiller(e) {
+        let brukernavn = inpBruker.value;
+        let alder = inpAlder.value;
+        let farge = inpFarge.value;
+        let navn = inpNavn.value;        
+        let ref = database.ref("spiller/" + brukernavn);
+        ref.set({ alder, farge, navn });
+    }
 }
