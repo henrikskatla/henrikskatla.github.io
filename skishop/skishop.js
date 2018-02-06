5d8b596a8227091d61558812cb54ddb0e9d46906
henrikskatla <henrik.s.katla@gmail.com>
2018-02-06 09:22:37 +0100

    ...

---
+++ skishop/skishop.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCXReCcJXX3_3T76CxjsHACsy1RCYKjRt4",
+    authDomain: "skishop-80488.firebaseapp.com",
+    databaseURL: "https://skishop-80488.firebaseio.com",
+    projectId: "skishop-80488",
+    storageBucket: "skishop-80488.appspot.com",
+    messagingSenderId: "827643093691"
+  };
